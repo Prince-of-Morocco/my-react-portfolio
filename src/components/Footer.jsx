d601c889994b1d917eb2c53a244a424d94d57eca
Bharath Raj 1024 <94532869+Prince-of-Morocco@users.noreply.github.com>
2026-04-15 21:08:51 +0530

    Update copyright year from 2025 to 2026

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ function Footer() {
             </div>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
               <p className="text-sm">
-                &copy; 2025 . All rights reserved.
+                &copy; 2026 . All rights reserved.
               </p>
               <p className="text-sm"> ❤️ </p>
             </div>
